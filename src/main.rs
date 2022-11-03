417cdd26ffb50f4ea7696aa40f61986a0cdbaa26
nate-sys <nathandawit555@gmail.com>
2022-11-03 19:30:52 +0300

    hide help by default

--- src/main.rs
+++ src/main.rs
@@ -13,33 +13,35 @@ use sycamore::prelude::*;
 use theme::Theme;
 use web_sys::{console, window};
 
-const LOCAL_STORAGE_KEY: &str = "journal_entries_sycamore";
+const ENTRY_KEY: &str = "journal_entries_sycamore";
 
 fn main() {
     sycamore::render(|cx| {
-        let storage = window().unwrap().local_storage().unwrap().unwrap();
-
         let app_state = AppState {
-            entry_groups: if let Ok(Some(e)) = storage.get_item(LOCAL_STORAGE_KEY) {
-                serde_json::from_str(&e).unwrap_or_else(|e| {
-                    console::error_1(&e.to_string().into());
+            entry_groups: {
+                let storage = window().unwrap().local_storage().unwrap().unwrap();
+                if let Ok(Some(e)) = storage.get_item(ENTRY_KEY) {
+                    serde_json::from_str(&e).unwrap_or_else(|e| {
+                        console::error_1(&e.to_string().into());
+                        Default::default()
+                    })
+                } else {
+                    console::log_1(&"No items in local storage".into());
                     Default::default()
-                })
-            } else {
-                console::log_1(&"No items in local storage".into());
-                Default::default()
+                }
             },
         };
         provide_context(cx, app_state);
 
         create_effect(cx, move || {
+            let storage = window().unwrap().local_storage().unwrap().unwrap();
             let app_state = use_context::<AppState>(cx);
             for group in app_state.entry_groups.get().iter() {
                 group.1.track();
             }
             storage
                 .set_item(
-                    LOCAL_STORAGE_KEY,
+                    ENTRY_KEY,
                     &serde_json::to_string(app_state.entry_groups.get().as_ref()).unwrap(),
                 )
                 .unwrap();
@@ -50,7 +52,7 @@ fn main() {
             !app_state.entry_groups.get().is_empty()
         });
 
-        let should_show_help = create_signal(cx, true);
+        let should_show_help = create_signal(cx, false);
         let show_help = |_| {
             should_show_help.set(true);
         };
